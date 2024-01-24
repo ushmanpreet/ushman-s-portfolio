79b623c2823d93fb68ef41c4085e3e6166611265
ushmanpreetsingh <ushmanpreetsingh@academic.rrc.ca>
2024-01-24 14:06:47 -0600

    ushmn

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Codin 1</h1>
+    
+      </header>
+    </div>
+  );
+}
+
+export default App;
